50aa0d2d8416c23ab0e66dde894a124f39e48893
John Glen Siy <johnglen_siy@berkeley.edu>
2022-02-25 11:26:21 -0800

    up to perms

--- proj1/enigma/Permutation.java
+++ proj1/enigma/Permutation.java
@@ -1,10 +1,11 @@
 package enigma;
 
 import static enigma.EnigmaException.*;
+import java.util.ArrayList;
 
 /** Represents a permutation of a range of integers starting at 0 corresponding
  *  to the characters of an alphabet.
- *  @author
+ *  @author John Glen Siy
  */
 class Permutation {
 
@@ -15,13 +16,27 @@ class Permutation {
      *  Whitespace is ignored. */
     Permutation(String cycles, Alphabet alphabet) {
         _alphabet = alphabet;
-        // FIXME
+        _cycles = new ArrayList<String>();
+        permInit(cycles);
     }
 
     /** Add the cycle c0->c1->...->cm->c0 to the permutation, where CYCLE is
      *  c0c1...cm. */
+    private void permInit(String cycles) {
+        String cycleSoFar = "";
+        for (char c : cycles.toCharArray()) {
+            if (c == ')') {
+                _cycles.add(cycleSoFar);
+                cycleSoFar = "";
+            }
+            else if (c != '(' && c != ' ') {
+                cycleSoFar += c;
+            }
+        }
+    }
+
     private void addCycle(String cycle) {
-        // FIXME
+        _cycles.add(cycle);
     }
 
     /** Return the value of P modulo the size of this permutation. */
@@ -35,30 +50,46 @@ class Permutation {
 
     /** Returns the size of the alphabet I permute. */
     int size() {
-        return 0; // FIXME
-    }
-
-    /** Return the result of applying this permutation to P modulo the
-     *  alphabet size. */
-    int permute(int p) {
-        return 0;  // FIXME
-    }
-
-    /** Return the result of applying the inverse of this permutation
-     *  to  C modulo the alphabet size. */
-    int invert(int c) {
-        return 0;  // FIXME
+        return _alphabet.size();
     }
 
     /** Return the result of applying this permutation to the index of P
      *  in ALPHABET, and converting the result to a character of ALPHABET. */
     char permute(char p) {
-        return 0;  // FIXME
+        for (String cycle : _cycles) {
+            int cLength = cycle.length();
+            for (int i = 0; i < cLength; i++) {
+                if (cycle.charAt(i) == p) {
+                    return cycle.charAt((i + 1) % cLength);
+                }
+            }
+        }
+        return p;
     }
 
     /** Return the result of applying the inverse of this permutation to C. */
     char invert(char c) {
-        return 0;  // FIXME
+        for (String cycle : _cycles) {
+            int cLength = cycle.length();
+            for (int i = 0; i < cLength; i++) {
+                if (cycle.charAt(i) == c) {
+                    return cycle.charAt((i - 1) % cLength);
+                }
+            }
+        }
+        return c;
+    }
+
+    /** Return the result of applying this permutation to P modulo the
+     *  alphabet size. */
+    int permute(int p) {
+        return permute(_alphabet.toChar(p));
+    }
+
+    /** Return the result of applying the inverse of this permutation
+     *  to  C modulo the alphabet size. */
+    int invert(int c) {
+        return permute(_alphabet.toChar(c));
     }
 
     /** Return the alphabet used to initialize this Permutation. */
@@ -74,6 +105,8 @@ class Permutation {
 
     /** Alphabet of this permutation. */
     private Alphabet _alphabet;
+
+    private ArrayList<String> _cycles;
 
     // FIXME: ADDITIONAL FIELDS HERE, AS NEEDED
 }
