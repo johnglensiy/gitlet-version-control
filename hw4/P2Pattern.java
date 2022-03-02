0ecfe40c258c5d8a42526c6a958ea97e8d27e510
John Glen Siy <johnglen_siy@berkeley.edu>
2022-03-01 23:34:49 -0800

    done hw4

--- hw4/P2Pattern.java
+++ hw4/P2Pattern.java
@@ -4,13 +4,14 @@
 
 public class P2Pattern {
     /* Pattern to match a valid date of the form MM/DD/YYYY. Eg: 9/22/2019 */
-    public static String P1 = ""; //FIXME: Add your regex here
+    public static String P1 = "^(0?[1-9]|1[012])/" +
+            "(0?[1-9]|[12]\\d|3[01])/((19|20)\\d{2})";//FIXME: Add your regex here
 
     /** Pattern to match 61b notation for literal IntLists. */
-    public static String P2 = ""; //FIXME: Add your regex here
+    public static String P2 = "^\\((\\d+\\,\\s*)+(\\d+)\\)"; //FIXME: Add your regex here
 
     /* Pattern to match a valid domain name. Eg: www.support.facebook-login.com */
-    public static String P3 = ""; //FIXME: Add your regex here
+    public static String P3 = "^(www.)?((\\w|\\w\\-\\w)+\\.)+(\\w{2,6})"; //FIXME: Add your regex here
 
     /* Pattern to match a valid java variable name. Eg: _child13$ */
     public static String P4 = ""; //FIXME: Add your regex here
